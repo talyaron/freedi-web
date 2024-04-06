be38dbbfced5a2618df0acc78c6aa91be4272e45
Vladislav Bykanov <vbykanov1989@outlook.com>
2024-04-06 23:52:19 +0200

    redirect link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,8 @@ import { redirect } from "next/navigation";
 
 export default function App() {
     useEffect(() => {
-        console.log("test");
-        redirect("/home");
+        redirect("/he/home");
     }, []);
 
-    return (
-        <div>
-            <h1>App Page</h1>
-        </div>
-    );
+    return <></>;
 }

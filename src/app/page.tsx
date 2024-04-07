6f9e03ed5c4405cfb8b20e0576a8907332beea61
Vladislav Bykanov <vbykanov1989@outlook.com>
2024-04-07 17:14:03 +0200

    add loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import "./appStyle.scss";
+
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
+import LoaderGlass from "@/components/loaders/loader-glass/LoaderGlass";
 
 export default function App() {
     useEffect(() => {
         redirect("/he/home");
     }, []);
 
-    return <></>;
+    return (
+        <div className="app">
+            <h1>{"Delib: Make Decisions Together"}</h1>
+            <LoaderGlass />
+            <h2>{("Please wait while the page loads")}</h2>
+        </div>
+    );
 }

cf0c0089d3d719f9b0844e86869860986a2f6059
Bar .A <bar7569@gmail.com>
2024-06-23 19:54:44 +0300

    changed footer

--- src/components/Pages/HomeSections/footer/HomeFooter.tsx
+++ src/components/Pages/HomeSections/footer/HomeFooter.tsx
@@ -11,15 +11,56 @@ import backgroundShape2 from "@/assets/Images/Backgroun shape 03.png";
 import nature from "@/assets/Images/nature.png";
 import flower2 from "@/assets/Images/flower02.png";
 
+
 // Custom Components
 import PageLinks from "@/components/top-navbar/PageLinks";
 import LogoWithName from "@/components/logo/LogoWithName";
+import ContactPhoneIcon from "@/components/icons/ContactPhoneIcon"
+import ContactEmailIcon from "@/components/icons/ContactEmailIcon";
+import ArrowUpIcon from "@/components/icons/ArrowUpIcon";
 
 export default function HomeFooter() {
 
     return (
-        <section className="footer">
-            <Image
+        <div className="footer">
+            <div className="footer__mainArea">
+                <div className="footer__topText">
+                    <p>Together, we can build a better future for everyone</p>
+                </div>
+                <div className="footer__contactDetails">
+                    <div className="footer__contactDetails__call">
+                        <ContactPhoneIcon />
+                        <div className="footer__contactDetails__text">
+                            <p>Call us</p>
+                            <p>+972-52-607-9419</p>
+                        </div>
+
+                    </div>
+                    <div className="footer__contactDetails__email">
+                        <ContactEmailIcon />
+                        <div className="footer__contactDetails__text">
+                            <p>Contact via Email</p>
+                            <p>tal.yaron@delib.org</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer__bottomText">
+                    <p>FreeDi Genuine Deliberation</p>
+                </div>
+            </div>
+            <hr />
+            <div className="footer__bottomArea">
+                <div className="footer__arrowUp">
+                    <ArrowUpIcon />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+{/* <Image
                 className="footer__backgroundShapeLeft"
                 alt="background-image-01"
                 src={backgroundShape}
@@ -38,13 +79,13 @@ export default function HomeFooter() {
                 className="footer__natureImage"
                 alt="nature-image"
                 src={nature}
-            />
-            <div className="footer__text">
-                <LogoWithName color="var(--button-primary)" size="1.2rem"/>
+            /> */}
+{/* <div className="footer__text">
+                <LogoWithName color="var(--button-primary)" size="1.2rem" /> */}
 
-                {/* <PageLinks isFooter={true} /> */}
+{/* <PageLinks isFooter={true} /> */ }
 
-                <p className="sectionText footer__text__credit">
+{/* <p className="sectionText footer__text__credit">
                     {("From the Institute for Deliberative Democracy")}
                 </p>
             </div>
@@ -52,7 +93,4 @@ export default function HomeFooter() {
                 className="footer__backgroundShapeRight"
                 alt="background-image-01"
                 src={backgroundShape}
-            />
-        </section>
-    );
-}
+            /> */}

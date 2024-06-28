a86ef33e3dd8808040c4c1a9daa68b4682973cbe
Bar .A <bar7569@gmail.com>
2024-06-28 20:36:23 +0300

    added who are we section

--- src/app/[lang]/home/page.tsx
+++ src/app/[lang]/home/page.tsx
@@ -22,6 +22,7 @@ import HomeFooter from "@/components/Pages/HomeSections/footer/HomeFooter";
 // Helpers
 import { getLanguageData } from "@/helpers/languages";
 import BenefitsSection from "@/components/Pages/HomeSections/benefits/BenefitsSection";
+import WhoAreWeSection from "@/components/Pages/HomeSections/whoAreWe/WhoAreWeSection";
 
 export default async function Home({
     params,
@@ -35,10 +36,10 @@ export default async function Home({
     const direction =
         params.lang === "he" || params.lang === "ar" ? "rtl" : "ltr";
 
-        const isHebrew = params.lang === "he";
+    const isHebrew = params.lang === "he";
 
     return (
-        <main className={isHebrew?"main he":"main en"}>
+        <main className={isHebrew ? "main he" : "main en"}>
             <Image
                 alt="Hero-Background"
                 src={heroBg}
@@ -78,11 +79,14 @@ export default async function Home({
                     direction={direction}
                 />
 
+                <WhoAreWeSection />
+                
                 <TeamworkSection
                     currentLang={currentLang}
                     direction={direction}
                 />
             </div>
+
 
             <HomeFooter />
         </main>

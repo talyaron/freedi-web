30244e1851ff90571830948fe5d4f30056fc16e3
Vladislav Bykanov <vbykanov1989@outlook.com>
2024-07-12 15:52:11 +0200

    Remove unused code

--- src/app/[lang]/home/page.tsx
+++ src/app/[lang]/home/page.tsx
@@ -1,21 +1,10 @@
 import React from "react";
 
-import Image from "next/image";
-
 // Styles
 import "./home.scss";
 
-// import Images
-import heroBg from "@/assets/Images/Hero-Background.png";
-
 // Custom Components
-import AboutSection from "@/components/Pages/HomeSections/about/AboutSection";
-import WelcomeHeader from "@/components/Pages/HomeSections/header/WelcomeHeader";
-import FeatureSection from "@/components/Pages/HomeSections/features/FeatureSection";
 import VideoSection from "@/components/Pages/HomeSections/videoSection/VideoSection";
-import ClientFeedbackSection from "@/components/Pages/HomeSections/feedback/ClientFeedbackSection";
-import AccessTodaySection from "@/components/Pages/HomeSections/join-today/AccessTodaySection";
-import TeamworkSection from "@/components/Pages/HomeSections/teamwork/TeamworkSection";
 import HomeFooter from "@/components/Pages/HomeSections/footer/HomeFooter";
 import HeroSection from "@/components/Pages/HomeSections/heroSection/HeroSection";
 import WhoAreWeSection from "@/components/Pages/HomeSections/whoAreWe/WhoAreWeSection";
@@ -27,62 +16,28 @@ import { getLanguageData } from "@/helpers/languages";
 export default async function Home({
 	params,
 }: Readonly<{
-    params: { lang: "he" | "en" | "ar" };
+	params: { lang: "he" | "en" | "ar" };
 }>) {
 	const langs = await getLanguageData();
 
 	const currentLang = langs[params.lang] || "en";
 
 	const direction =
-        params.lang === "he" || params.lang === "ar" ? "rtl" : "ltr";
+		params.lang === "he" || params.lang === "ar" ? "rtl" : "ltr";
 
 	const isHebrew = params.lang === "he";
 
 	return (
 		<main className={isHebrew ? "main he" : "main en"}>
-			<Image
-				alt="Hero-Background"
-				src={heroBg}
-				loading="eager"
-				priority
-				style={{
-					width: "100%",
-					height: "20rem",
-					position: "absolute",
-					top: 0,
-					zIndex: -10,
-				}}
-			/>
-			<WelcomeHeader currentLang={currentLang} />
-
 			<div className="main__body">
-				<AboutSection currentLang={currentLang} direction={direction} />
-
-				<FeatureSection
-					currentLang={currentLang}
-					direction={direction}
-				/>
+				<HeroSection />
 
 				<WhyUsSection currentLang={currentLang} direction={direction} />
 
 				<VideoSection currentLang={currentLang} direction={direction} />
 
-				<ClientFeedbackSection />
-
-				<AccessTodaySection
-					currentLang={currentLang}
-					direction={direction}
-				/>
-
 				<WhoAreWeSection />
-
-				<TeamworkSection
-					currentLang={currentLang}
-					direction={direction}
-				/>
 			</div>
-
-			<HeroSection />
 
 			<HomeFooter />
 		</main>
